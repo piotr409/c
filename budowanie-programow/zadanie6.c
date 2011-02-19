393fece416281f340dcf0d9b8e95144de76054d6
Piotr Kulas <piotr409@gmail.com>
2011-02-19 23:05:26 +0000

    Dodano zadanie 6.

---
+++ budowanie-programow/zadanie6.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+float kwadratowy(float pierwiastek){
+  if(pierwiastek==.0)
+  {
+      return .0;
+
+  } return sqrt(pierwiastek);
+}
+
+int main(){
+
+  float b;
+  scanf("%f",&b);
+  if(b<0.0)
+ {
+    printf("Wystąpił błąd, liczba %lf jest mniejsza od 0.\n",b);
+    return 0;
+ }
+  printf("Pierwiastek z liczby %lf wynosi %lf\n",b,kwadratowy(b));
+  return 0;
+}
