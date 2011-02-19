8eb177ffec8aef8a1557c46b00a931361eb4303e
Piotr Kulas <piotr409@gmail.com>
2011-02-19 23:18:36 +0000

    Poprawa wyglądu kodu.

--- budowanie-programow/zadanie6.c
+++ budowanie-programow/zadanie6.c
@@ -12,10 +12,10 @@ int main() {
 
   float b;
   scanf("%f", &b);
-  if (b < 0.0) {
+  if (b < .0) {
     printf("Wystąpił błąd, liczba %lf jest mniejsza od 0.\n", b);
     return 0;
   }
-  printf("Pierwiastek z liczby %lf wynosi %lf\n",b, kwadratowy(b));
+  printf("Pierwiastek z liczby %lf wynosi %lf\n", b, kwadratowy(b));
   return 0;
 }
