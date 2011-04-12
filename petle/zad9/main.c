73bfebfe7a5a6a173b846e92952f5d9315c295c4
Piotr Kulas <piotr409@gmail.com>
2011-04-12 16:03:26 +0200

    Użyto indent -kr

--- petle/zad9/main.c
+++ petle/zad9/main.c
@@ -6,19 +6,22 @@ int main(int argc, char *argv[])
 {
     FILE *in_handle;
     char bufor[BUFSIZ];
-    unsigned int ui[10000], sui; int it  = sui = 0;
+    unsigned int ui[10000], sui;
+    int it = sui = 0;
     double x[10000], y[10000], licznik, mianownik;
-  in_handle = fopen(argv[1], "r");
-	licznik = mianownik = 0;
-  while (fgets(bufor, BUFSIZ, in_handle) != NULL) {
-    sscanf(bufor, "%u %lf %lf", &ui[it], &x[it], &y[it]);
-	if(ui[it]!=0) {
-	spalanie(ui[it], x[it], y[it]);
-	licznik = licznik + ( x[it] * y[it] ); sui += ui[it]; mianownik += x[it];
-	it++;
+    in_handle = fopen(argv[1], "r");
+    licznik = mianownik = 0;
+    while (fgets(bufor, BUFSIZ, in_handle) != NULL) {
+	sscanf(bufor, "%u %lf %lf", &ui[it], &x[it], &y[it]);
+	if (ui[it] != 0) {
+	    spalanie(ui[it], x[it], y[it]);
+	    licznik = licznik + (x[it] * y[it]);
+	    sui += ui[it];
+	    mianownik += x[it];
+	    it++;
 	}
-  }
-	przetworz_informacje(licznik, mianownik,sui);
-  fclose(in_handle);
+    }
+    przetworz_informacje(licznik, mianownik, sui);
+    fclose(in_handle);
     return EXIT_SUCCESS;
 }
--- petle/zad9/plik.c
+++ petle/zad9/plik.c
@@ -4,18 +4,20 @@
 
 void spalanie(unsigned int u, double x, double y)
 {
- double spal = (x * 100)/(double)u;
-	printf("Zatankowane : %.2f litrów, koszt 1 litra : %.2f \n", x, y);
+    double spal = (x * 100) / (double) u;
+    printf("Zatankowane : %.2f litrów, koszt 1 litra : %.2f \n", x, y);
     printf("Spalanie paliwa na 100km : %.2f litrów\n", spal);
- printf("Koszt przejechania 100km : %.2f zł\n\n", spal * y );
-    
+    printf("Koszt przejechania 100km : %.2f zł\n\n", spal * y);
+
 }
-void przetworz_informacje(double l, double m, unsigned int ui){
-	printf("--------------------------------\n");	
-	printf(" Koszt 1 litra paliwa : %.2f zł\n", l/m);
-	printf(" Łączna ilość przejechanych kilometrów : %u \n", ui);
-	printf(" Łączna ilość zatankowanego paliwa : %.2f \n", m);
-	printf(" Koszt łącznego tankowania : %.2f zł\n", m * (l/m));
-	printf(" Średnie spalanie na 100km : %.2f litrów \n", (m*100)/(double)ui);
+void przetworz_informacje(double l, double m, unsigned int ui)
+{
+    printf("--------------------------------\n");
+    printf(" Koszt 1 litra paliwa : %.2f zł\n", l / m);
+    printf(" Łączna ilość przejechanych kilometrów : %u \n", ui);
+    printf(" Łączna ilość zatankowanego paliwa : %.2f \n", m);
+    printf(" Koszt łącznego tankowania : %.2f zł\n", m * (l / m));
+    printf(" Średnie spalanie na 100km : %.2f litrów \n",
+	   (m * 100) / (double) ui);
 
 }
