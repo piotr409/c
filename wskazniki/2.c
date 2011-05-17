2516daec8df37bba2686019f4dbcd73612544d65
Piotr Kulas <piotr409@gmail.com>
2011-05-17 17:02:54 +0200

    Dodano zadanie 2

--- wskazniki/2.c
+++ wskazniki/2.c
@@ -1,28 +1,42 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int main()
 {
-    int t[10] = { 1, 3, 5, 7, 9, 0, 2, 4, 6, 8 };
-    int *a[10];
 
+    int t[4] = { 2, 1, 8, 3 };
+    int a[4] = { 0, 1, 2, 3 };
     int i, j;
 
-    for (i = 0; i < 10; i++) {
-	a[i] = &t[i];
+    for (i = 0; i < 4; i++) {
+	printf("%d ", t[i]);
     }
 
-    for (i = 0; i < 10; i++) {
-	for (j = i + 1; j < 10; j++) {
-	    if (*a[i] > *a[j]) {
-		int *temp;
+    printf("\n");
+
+
+    for (i = 0; i < 4; i++) {
+	for (j = i + 1; j < 4; j++) {
+	    if (t[i] > t[j]) {
+		int temp;
 		temp = a[i];
 		a[i] = a[j];
 		a[j] = temp;
 	    }
+
 	}
     }
 
-    for (i = 0; i < 10; i++) {
-	printf("%d ", *a[i]);
+    for (i = 0; i < 4; i++) {
+	printf("%d ", a[i]);
     }
+
+    printf("\n");
+
+    for (i = 0; i < 4; i++) {
+	printf("%d ", t[a[i]]);
+    }
+
+
+    printf("\n");
+    return 0;
 }
